edc90cfa2df10ed7287ea284749b1465cf47c5eb
Victor Blackbird <113912794+V-Blackbird@users.noreply.github.com>
2023-08-10 18:29:50 +0200

    Update front_pic_coordinates.h with Vill

--- src/data/pokemon_graphics/front_pic_coordinates.h
+++ src/data/pokemon_graphics/front_pic_coordinates.h
@@ -2060,6 +2060,11 @@ const struct MonCoords gMonFrontPicCoords[] =
         .size = MON_COORDS_SIZE(24, 56),
         .y_offset = 6,
     },
+    [SPECIES_VILL] =
+    {
+        .size = MON_COORDS_SIZE(64, 64),
+        .y_offset = 0,
+    },
     [SPECIES_EGG] =
     {
         .size = MON_COORDS_SIZE(24, 24),
